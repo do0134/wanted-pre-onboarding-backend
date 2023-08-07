d86eb24ac0807a6367ea4116f7ddf3d988afc291
do0134 <do0134@naver.com>
2023-08-07 17:32:41 +0900

    [feat] : 회원가입 함수 개발

--- preonboarding/accounts/views.py
+++ preonboarding/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from argon2 import PasswordHasher
+from django.http import JsonResponse
+from .serializers import UserSerializer
 
 # Create your views here.
+@api_view(["Post"])
+def sign_up(request):
+    email = request.email
+    password = request.password
+
+    if "@" not in email:
+        context = {
+            'message' : "올바른 이메일 형식을 입력해주세요"
+        }
+        return JsonResponse(context)
+    elif len(password) < 8:
+        context = {
+            'message' : "비밀번호는 8자리 이상이어야 합니다."
+        }
+        return JsonResponse(context)
+    else:
+        crypto = PasswordHasher.hash(password)
+        new_user = {
+            'email' : email,
+            'password' : crypto,
+        }
+        serializer = UserSerializer(data=new_user)
+        
+        if serializer.is_valid(raise_exception=True):
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
--- preonboarding/preonboarding/settings.py
+++ preonboarding/preonboarding/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
 
+    'rest_framework',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
