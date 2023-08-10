82f96ff6b1d603bc28a3dab6359f18a135d2cf93
do0134 <do0134@naver.com>
2023-08-10 11:35:47 +0900

    [refact] 불필요한 import 삭제

--- preonboarding/articles/urls.py
+++ preonboarding/articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    
+    path('', views.article_list_or_create),
+    path('<int:article_pk>', views.article_detail_or_update_or_delete),
 ]
--- preonboarding/articles/views.py
+++ preonboarding/articles/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Article
+from django.http import JsonResponse
+from .serializers import ArticleSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+def article_list():
+    pass
+
+def article_create():
+    pass
+
+@api_view(["GET", "POST"])
+def article_list_or_create(request):
+    pass
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail_or_update_or_delete(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+
+    def article_detail():
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update_article():
+        if request.user == article.user:
+            serializer = ArticleSerializer(instance=article, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+
+    def delete_article():
+        if request.user == article.user:
+            article.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    if request.method == 'GET':
+        return article_detail()
+    elif request.method == 'PUT':
+        if request.user == article.user:
+            return update_article()
+    elif request.method == 'DELETE':
+        if request.user == article.user:
+            return delete_article()
