b2c8a9e963067ebcaa37a6868debf31cebb1de25
do0134 <do0134@naver.com>
2023-08-10 14:46:12 +0900

    [feat] : article_list 기능 구현

--- preonboarding/articles/views.py
+++ preonboarding/articles/views.py
@@ -3,22 +3,35 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Article
-from django.http import JsonResponse
 from .serializers import ArticleSerializer
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 # Create your views here.
 
-def article_list():
-    pass
 
-def article_create():
-    pass
 
 @api_view(["GET", "POST"])
 def article_list_or_create(request):
-    pass
+    def article_list():
+        articles = Article.objects.all()
+        page = int(request.GET.get('page') or 1)
+        paginator = Paginator(articles, 10)
+        article_list = paginator.get_page(page)
+        context = {
+            "article_list" : article_list
+        }
+
+        return Response(context,status=status.HTTP_200_OK)
+
+    def article_create():
+        
+        pass
+    
+    if request.method == "GET":
+        return article_list()
+    elif request.method == "POST":
+        return article_create()
+    
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_detail_or_update_or_delete(request, article_pk):
@@ -48,3 +61,4 @@ def article_detail_or_update_or_delete(request, article_pk):
     elif request.method == 'DELETE':
         if request.user == article.user:
             return delete_article()
+   
