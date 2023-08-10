6e9663fbcebbffc74dc8fda4e87dff1fdef58f26
do0134 <do0134@naver.com>
2023-08-10 16:23:05 +0900

    [feat] : pagination 테스트 및 구현

--- preonboarding/articles/tests.py
+++ preonboarding/articles/tests.py
@@ -1,3 +1,79 @@
 from django.test import TestCase
-
+from .models import Article
+from django.core.paginator import Paginator
+from .serializers import ArticleSerializer
+from django.contrib.auth import get_user_model
+from accounts.serializers import UserSerializer, UserArticleSerializer
+from django.shortcuts import get_object_or_404
 # Create your tests here.
+
+User = get_user_model()
+
+class ArticleTest(TestCase):
+    # 테스트 용 유저 생성
+    def create_user(self):
+        data = {"email" : "dodo@naver.com", "password" : "12345678"}
+        serializer = UserSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+
+    # 테스트용 게시글 생성 함수
+    def article_create(self):
+        # create_user로 만들어진 유저를 받은 다음,
+        user = get_object_or_404(User, id=1)
+        # 테스트 데이터를 넣고
+        data = {
+            "title" : "1234",
+            "content" : "1234",
+        }
+       # 저장
+        serializer = ArticleSerializer(data=data)
+        # valid 데이터라면 저장
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.save(user=user)
+        
+        # 만약 article을 조회할 수 있다면 OK 아니면 AssertionError가 난다.
+        try : 
+            article = Article.objects.get(pk=new_data.pk)
+        except : 
+            raise AssertionError
+
+    # 게시글 리스트 페이지네이션 테스트
+    def test_article_list(self):
+        # 유저 생성
+        self.create_user()
+        # 게시글을 총 몇 번 생성할지
+        num = 31
+        # 한 페이지에 게시글을 몇 개 보여줄지
+        article_cnt = 5
+        # num만큼 게시글 생성
+        try: 
+            for _ in range(num):
+                self.article_create()
+        except:
+            raise AssertionError
+        
+        # 생성된 게시글 목록을 가져오고
+        articles = Article.objects.all().order_by('-pk')
+        paginator = Paginator(articles, article_cnt)
+        # max_len은 총 게시글 // 한 페이지당 보이는 게시글 + 1
+        max_len = num // article_cnt + 1
+        # 반복문을 통해서 페이지마다 정확한 수의 게시글을 보여주는지 확인
+        for i in range(1,max_len):
+            page = i
+            article_list = paginator.get_page(page)
+            serializer = ArticleSerializer(article_list,many=True)
+
+            if i != max_len:
+                self.assertTrue(len(serializer.data) == article_cnt)
+            # 마지막 페이지만 예외처리 해준다.
+            else:
+                self.assertTrue(len(serializer.data) == num%article_cnt)
+        
+        
+        
+
+        
+        
+       
--- preonboarding/articles/views.py
+++ preonboarding/articles/views.py
@@ -13,18 +13,15 @@ from django.core.paginator import Paginator
 @api_view(["GET", "POST"])
 def article_list_or_create(request):
     def article_list():
-        articles = Article.objects.all()
+        articles = Article.objects.all().order_by('-pk')
         page = int(request.GET.get('page') or 1)
-        paginator = Paginator(articles, 10)
+        paginator = Paginator(articles, 5)
         article_list = paginator.get_page(page)
-        context = {
-            "article_list" : article_list
-        }
+        serializer = ArticleSerializer(article_list,many=True)
 
-        return Response(context,status=status.HTTP_200_OK)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
     def article_create():
-        
         pass
     
     if request.method == "GET":
@@ -33,32 +30,7 @@ def article_list_or_create(request):
         return article_create()
     
 
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_detail_or_update_or_delete(request, article_pk):
-    article = get_object_or_404(Article, pk=article_pk)
-
-    def article_detail():
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-
-    def update_article():
-        if request.user == article.user:
-            serializer = ArticleSerializer(instance=article, data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data)
-
-    def delete_article():
-        if request.user == article.user:
-            article.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    if request.method == 'GET':
-        return article_detail()
-    elif request.method == 'PUT':
-        if request.user == article.user:
-            return update_article()
-    elif request.method == 'DELETE':
-        if request.user == article.user:
-            return delete_article()
-   
+    pass
